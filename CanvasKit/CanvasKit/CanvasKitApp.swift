c7caa83c89367deae8d353a32cd0807dfe506ad5
lukeatki <luke24aal@gmail.com>
2024-02-23 13:19:29 -0500

    initial project commit

---
+++ CanvasKit/CanvasKit/CanvasKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasKitApp.swift
+//  CanvasKit
+//
+//  Created by Luke Atkins on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CanvasKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasKit/CanvasKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CanvasKit
+//
+//  Created by Luke Atkins on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
