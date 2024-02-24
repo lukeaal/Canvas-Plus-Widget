d4734da3793b1bd7bf5c22109e1fedc9f42ff4ef
lukeatki <luke24aal@gmail.com>
2024-02-24 00:09:02 -0500

    started creating model and structs for canvas api; waiting for personal token to test from UITS

---
+++ CanvasKit/CanvasKit/CanvasAPIModel.swift
@@ -0,0 +1,36 @@
+//
+//  CanvasAPIModel.swift
+//  CanvasKit
+//
+//  Created by Luke Atkins on 2/23/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+class CanvasAPIModel {
+    
+    
+}
+
+
+struct Assignment {
+    let id: Int
+    let name: String
+    let description: String
+}
+
+// Main idea --> **get user, get user courses id's, get assignment list for each course, present totality to user**
+// https://canvas.instructure.com/doc/api/assignments.html#method.assignments_api.user_index
+// Steps...
+// 1. get user api Doc:
+//
+// 2. get users courses id's Doc:
+//
+// 3. get courses assignmnt list doc:
+// https://canvas.instructure.com/doc/api/assignments.html#method.assignments_api.index
+// 4. assignment struct Doc:
+// https://canvas.instructure.com/doc/api/assignments.html
+
